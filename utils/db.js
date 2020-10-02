f73616cf32d1ab63b0cd553e1fbaf95152391135
ollaymkenya <muriithioliver11@outlook.com>
2020-10-02 17:56:45 +0300

    This will make some changes on how I connect to db

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,13 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
+    user: "uhzqnzjxlgangt",
     password: "olimkenya",
-    host: "localhost",
+    host: "ec2-18-211-86-133.compute-1.amazonaws.com",
     port: 5432,
-    database: "enginear"
+    database: "decupenudj2sae",
+    password: "c3ccb89f8cfb97855f837dfd1a3949efd391c775fab8b9c8088d39e12de8409e",
+    URI: "postgres://uhzqnzjxlgangt:c3ccb89f8cfb97855f837dfd1a3949efd391c775fab8b9c8088d39e12de8409e@ec2-18-211-86-133.compute-1.amazonaws.com:5432/decupenudj2sae"
 })
 
 module.exports = pool;
